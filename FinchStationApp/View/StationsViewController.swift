cf1cdc0f1502c5fbc2286bd4f9c073dd2fbf04cf
James Lok <cjames.lok@hotmail.com>
2022-06-10 14:53:00 -0400

    view fixed

--- FinchStationApp/View/StationsViewController.swift
+++ FinchStationApp/View/StationsViewController.swift
@@ -17,42 +17,29 @@ class StationsViewController: UIViewController, UITableViewDelegate {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
+//        self.view.backgroundColor = UIColor.red
         setupUI()
-        
         presenter.viewDidLoad()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        setupNavItem()
     }
-//
-//    override func loadView() {
-//        super.loadView()
-//        setupUI()
-//      }
 
-    
     
     // MARK: - Properties
     var presenter: StationsViewPresenter!
     var stations: [Station] = []
     
-    let tv = UITableView()
-    
-    
-//    lazy var tableView: UITableView = {
-//        let tableView = UITableView(frame: view.bounds)
-//        tableView.dataSource = self
-//        tableView.delegate = self
-//
-//        tableView.tableFooterView = UIView()
-//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-////        tableView
-////            .translatesAutoresizingMaskIntoConstraints = false
-//        return tableView
-//    }()
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.tableFooterView = UIView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     
     lazy var placeholderLabel: UILabel = {
         let label = UILabel()
@@ -66,25 +53,15 @@ class StationsViewController: UIViewController, UITableViewDelegate {
     // MARK: - UI Setup
     func setupUI() {
         
-        let tableView = UITableView(frame: view.bounds)
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.tableFooterView = UIView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-//        tableView
-//            .translatesAutoresizingMaskIntoConstraints = false
-
         
         view.addSubview(tableView)
-        tableView.backgroundColor = UIColor.red
-//
-//        tableView.translatesAutoresizingMaskIntoConstraints = false
-//        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-//        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-//
+        self.title = "Stations"
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
     }
 
 }
@@ -95,7 +72,7 @@ extension StationsViewController: StationsView {
     func onStationsRetrieval(stations: [Station]) {
         print("View recieves the result from the Presenter.")
         self.stations = stations
-//        self.tableView.reloadData()
+        self.tableView.reloadData()
     }
     
 
